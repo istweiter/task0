59a61a17b3bf67843cc44542819e5081042e8d49
Nikita <istweiter@gmail.com>
2022-09-17 12:50:51 +0400

    first blood 2

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tkinter as tk
-window=tk._Tk()
+window=tk.Tk()
 window.geometry("300x300")
-windows.mainloop()
+def onclick():
+    print(1)
+btn1=tk.Button(text="нажми меня",command=onclick)
+btn1.grid(row=0,column=0)
+Chkvar=tk.IntVar()
+Chkbtn=tk.Checkbutton(text="выбор лампочки",variable=Chkvar,onvalue=1,offvalue=0)
+Chkbtn.grid(row=1,column=0)
+Scl=tk.Scale(label="яркость",from_=1,to=100,orient=tk.HORIZONTAL,length=200,width=10)
+scl.grid(row=2,column=0)
+
+window.mainloop()
