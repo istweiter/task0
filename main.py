57d997f0aa73a31e8dd3046132004bce9b795e51
Nikita <istweiter@gmail.com>
2022-09-17 12:51:47 +0400

    first blood 2v2

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ Chkvar=tk.IntVar()
 Chkbtn=tk.Checkbutton(text="выбор лампочки",variable=Chkvar,onvalue=1,offvalue=0)
 Chkbtn.grid(row=1,column=0)
 Scl=tk.Scale(label="яркость",from_=1,to=100,orient=tk.HORIZONTAL,length=200,width=10)
-scl.grid(row=2,column=0)
+Scl.grid(row=2,column=0)
 
 window.mainloop()
